79397750680138061d30b3a965cec41daf63915d
sxt <sxtsyf111@163.com>
2023-07-25 13:45:44 +0800

    1

---
+++ SCNViewDemo/SCNViewDemoUITests/SCNViewDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SCNViewDemoUITestsLaunchTests.swift
+//  SCNViewDemoUITests
+//
+//  Created by 宋秀腾 on 2023-07-22.
+//
+
+import XCTest
+
+final class SCNViewDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
